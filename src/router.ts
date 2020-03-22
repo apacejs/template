b7b11ce7628904a31fbd699eab6c6dad1854cd04
dkvirus <3548310748@qq.com>
2020-03-22 09:42:13 +0800

    fix: 审计日志挪到中间件中,避免jwt挡掉的请求没法记录日志

--- src/router.ts
+++ src/router.ts
@@ -1,8 +1,6 @@
 import { Application } from 'express'
-import audit from './route/audit.route'
 import AdminResponse from './util/AdminResponse'
 
 export default function (app: Application) {
-    app.use(audit.saveAudit)
     app.get('/', (req, res) => res.json(AdminResponse.success('hello world')))
 }
